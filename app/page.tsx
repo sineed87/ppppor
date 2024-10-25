43d1da0e6a08a595793a4dfe3974d4f44ff0f7a7
Denis <denis.gojkovic@gmail.com>
2024-10-25 17:10:36 +0200

    Describe the changes you made

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,11 @@ import React, { useEffect, useRef } from 'react';
 import Header3 from './components/Header3';
 import Header from './components/Header';
 import Header2 from './components/Header2';
-import LocomotiveScroll from 'locomotive-scroll';
 
 export default function Home() {
   const scrollRef = useRef<HTMLDivElement | null>(null); // Ensure TypeScript type
 
-  useEffect(() => {
-    // Ensure LocomotiveScroll runs only on the client
-    if (typeof window !== "undefined" && scrollRef.current) {
-      const scroll = new LocomotiveScroll({
-        el: scrollRef.current,
-        smooth: true,
-      });
-
-      // Clean up on component unmount
-      return () => {
-        scroll.destroy();
-      };
-    }
-  }, []);
-
+  // No need for useEffect related to LocomotiveScroll
   return (
     <div ref={scrollRef} className="font-work">
       <Header3 />
